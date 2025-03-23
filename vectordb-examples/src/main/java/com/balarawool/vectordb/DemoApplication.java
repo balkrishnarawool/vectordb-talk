2f4ea09083dbb772cf4682f152408a8fd64eebf1
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2025-03-23 23:09:26 +0530

    some small changes

--- vectordb-examples/src/main/java/com/balarawool/vectordb/DemoApplication.java
+++ vectordb-examples/src/main/java/com/balarawool/vectordb/DemoApplication.java
@@ -28,6 +28,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+///
+/// Don't forget --add-modules jdk.incubator.vector
 @SpringBootApplication
 public class DemoApplication {
 
